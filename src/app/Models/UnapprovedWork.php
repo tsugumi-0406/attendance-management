4774a22b22c089c32bece7032be09e6631c4860e
tsugumi <vampire5.9.jigsaw@gmail.com>
2026-01-28 14:53:13 +0900

    ユーザーの申請一覧の表示・申請詳細画面への遷移実装

--- src/app/Models/UnapprovedWork.php
+++ src/app/Models/UnapprovedWork.php
@@ -10,4 +10,9 @@ class UnapprovedWork extends Model
     use HasFactory;
 
     protected $fillable = ['work_id', 'user_id', 'date', 'attendance', 'leaving', 'remarks'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
