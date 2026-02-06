bb3f2bc1adfe4acd8078be63dc10de22a8c20916
tsugumi <vampire5.9.jigsaw@gmail.com>
2026-02-06 23:14:36 +0900

    勤怠の変更申請を承認無しで変更できるようにした

--- src/app/Http/Controllers/AdministratorAttendanceDetailController.php
+++ src/app/Http/Controllers/AdministratorAttendanceDetailController.php
@@ -3,11 +3,81 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Work;
+use App\Models\BreakTime;
+use App\Models\User;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+use App\Http\Requests\UserAttendanceDetailRequest;
+
 
 class AdministratorAttendanceDetailController extends Controller
 {
-    public function detail(Request $request)
+    public function detail($id)
     {
-        return view('administrator_attendance_detail');
+        $work = Work::where('id', $id)
+                ->firstOrFail();
+
+        $user = User::where('id', $work->user_id)
+                ->first();
+        
+        $breaks = BreakTime::where('user_id', $user->id)
+            ->where('date', $work->date)
+            ->get();
+
+        return view('administrator_attendance_detail',compact('work', 'user', 'breaks'));
+    }
+
+
+
+    public function apply(UserAttendanceDetailRequest $request)
+    {
+        $work = Work::findOrFail($request->work_id);
+
+        $targetUserId = $work->user_id;
+
+        $now = CarbonImmutable::now();
+        $date = $now->toDateString();
+
+        $work->update([
+            'attendance' => $request->attendance,
+            'leaving' => $request->leaving,
+            'remarks' => $request->remarks,
+            'update' => 'done',
+            'application_date' => $date,
+        ]);
+
+        $break_datas = $request->break_requests;
+
+        if($break_datas == null){
+
+        }else{
+            foreach($break_datas as $break_data){
+                $break_id = $break_data['break_id'];
+
+                $start = $break_data['start'];
+
+                if($break_data['stop'] == '--:--'){
+                    $stop = null;
+                }else{
+                    $stop = $break_data['stop'];
+                }
+
+                BreakTime::find($break_id)->update([
+                    'start' => $start,
+                    'stop' => $stop,
+                    'update' => 'done',
+                    'application_date' => $now,
+                ]);
+            }
+        }
+
+        dd(
+  now()->toDateTimeString(),
+  now('Asia/Tokyo')->toDateTimeString(),
+  config('app.timezone')
+);
+
+        return redirect('/admin/attendance/detail/' . $work->id);
     }
 }
--- src/resources/views/administrator_attendance_detail.blade.php
+++ src/resources/views/administrator_attendance_detail.blade.php
@@ -6,39 +6,129 @@
 
 @section('content')
     <div class="main__inner">
+
         <div class="main-title">
             <div class="marin-title__div">a</div>
             <h1 class="main-title__sentence"> 勤怠詳細</h1>
         </div>
-        <form action="" class="form">
+
+
+        <form action="/admin/attendance/correction/apply" class="form" method="post">
+            @csrf
             <div class="form-inner">
                 <div class="form-line">
                     <p class="form-item">名前</p>
-                    <p class="form-data__name">勤務者名</p>
+                    <p class="form-data__name">{{ $user->name }}</p>
                 </div>
+
+
                 <div class="form-line">
+                    @php
+                        $work_day = Carbon\Carbon::parse($work->date);
+                    @endphp
                     <p class="form-item">日付</p>
-                    <p class="form-data__year">勤務年</p>
-                    <p class="form-data__date">勤務日付</p>
+                    <p class="form-data__year">{{ $work_day->format('Y'); }}年</p>
+                    <p class="form-data__date">{{ $work_day->format('n'); }}月{{ $work_day->format('j'); }}日</p>
                 </div>
+
+
                 <div class="form-line">
+                    @php
+                        $work_status = $work->update;
+
+                        $break_request = [];
+                    @endphp
+
+
+                    
+                    @php
+                        $work_id = $work->id;
+
+                        $work_attendance = Carbon\Carbon::parse($work->attendance)->format('H:i');
+
+                        if($work->leaving == null){
+                            $work_leaving = '--:--';
+                        }else{
+                            $work_leaving = Carbon\Carbon::parse($work->leaving)->format('H:i');
+                        }
+
+                        if($work->remarks == null){
+                            $work_remarks = '';
+                        }else{
+                            $work_remarks = $work->remarks;
+                        }
+                    @endphp
+                    <input type="text" hidden value="{{ $work_id }}" name="work_id">
                     <p class="form-item">出勤・退勤</p>
-                    <input type="text" class="form-data__attendance" value=出勤時間></input>
+                    <input type="text" class="form-data__attendance" value="{{ $work_attendance }}" name="attendance">
                     <p class="form-data__mark">～</p>
-                    <input type="text" class="form-data__leaving" value=退勤時間></input>
+                    <input type="text" class="form-data__leaving" value="{{ $work_leaving }}" name="leaving">
                 </div>
-                <div class="form-line">
-                    <p class="form-item">休憩</p>
-                    <input type="text" class="form-data__break-start" value=休憩開始時間></input>
-                    <p class="form-data__mark">～</p>
-                    <input type="text" class="form-data__break-end" value=休憩終了時間></input>
-                </div>
+
+
+                
+                @foreach($breaks as $index => $break)
+                    <div class="form-line">
+                        @php
+                            $break_id = $break->id;
+
+                            $break_start = Carbon\Carbon::parse($break->start)->format('H:i');
+
+                            if($break->stop == null){
+                                $break_stop = '--:--';
+                            }else{
+                                $break_stop = Carbon\Carbon::parse($break->stop)->format('H:i');
+                            }    
+                        @endphp
+                        <input type="text" hidden value="{{ $break_id }}" name="break_requests[{{ $index }}][break_id]">
+                        <p class="form-item">休憩</p>
+                        <input type="text" class="form-data__break-start" value="{{ $break_start }}" name="break_requests[{{ $index }}][start]">
+                        <p class="form-data__mark">～</p>
+                        <input type="text" class="form-data__break-end" value="{{ $break_stop }}" name="break_requests[{{ $index }}][stop]">
+                    </div>
+                @endforeach
+
+
                 <div class="form-line">
                     <p class="form-item">備考</p>
-                    <textarea class="form-data__remarks">修正理由</textarea>
+                    <textarea class="form-data__remarks" name="remarks">{{ $work_remarks }}</textarea>
                 </div>
             </div>
-            <input type="button" value="修正" class="form-button">
+
+            <div class="error">
+                <br>
+                    @error('attendance')
+                        {{ $errors->first('attendance') }}
+                    @enderror
+                <br>
+                    @error('leaving')
+                        {{ $errors->first('leaving') }}
+                    @enderror
+                <br>
+                    @error('break_requests')
+                        {{ $errors->first('break_requests') }}
+                    @enderror
+                <br>
+                    @if ($errors->has('break_requests.*.start') || $errors->has('break_requests.*.stop'))
+                        @foreach ($errors->get('break_requests.*.start') as $messages)
+                            @foreach ($messages as $message)
+                                <div>{{ $message }}</div>
+                            @endforeach
+                        @endforeach
+
+                        @foreach ($errors->get('break_requests.*.stop') as $messages)
+                            @foreach ($messages as $message)
+                                <div>{{ $message }}</div>
+                            @endforeach
+                        @endforeach
+                    @endif
+                <br>
+                    @error('remarks')
+                        {{ $errors->first('remarks') }}
+                    @enderror
+            </div>
+            <input type="submit" value="修正" class="form-button">
         </form>
     </div>
 @endsection
+
