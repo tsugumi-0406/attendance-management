fa3a66c05ea618fc9dd8e732420e2b2fba0e38ae
tsugumi <vampire5.9.jigsaw@gmail.com>
2026-02-10 13:07:05 +0900

    バリデーションメッセージ修正

--- src/tests/Feature/UserLoginTest.php
+++ src/tests/Feature/UserLoginTest.php
@@ -68,7 +68,7 @@ class UserLoginTest extends TestCase
 
         $response->assertSessionHasErrors(['email']);
         $this->assertEquals(
-            'ログイン情報が登録されていません。',
+            'ログイン情報が登録されていません',
             session('errors')->first('email')
         );
     }
